e4a173a47b565987ff9c4abca73f2e00ab796d53
Ahliko <killian.guillemot@ynov.com>
2025-01-07 15:20:44 +0100

    Add Files

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::env;
+use warp::Filter;
+
+#[derive(Serialize, Deserialize)]
+struct HeadersResponse {
+    headers: HashMap<String, String>,
+}
+
+#[tokio::main]
+async fn main() {
+    let headers_route = warp::path!("ping")
+        .and(warp::get())
+        .and(warp::header::headers_cloned())
+        .map(|headers: warp::http::HeaderMap| {
+            let mut headers_map = HashMap::new();
+            for (key, value) in headers.iter() {
+                headers_map.insert(
+                    key.to_string(),
+                    value.to_str().unwrap_or("Invalid UTF-8").to_string(),
+                );
+            }
+
+            let response = HeadersResponse {
+                headers: headers_map,
+            };
+            warp::reply::with_header(
+                warp::reply::json(&response),
+                "Content-Type",
+                "application/json",
+            )
+        });
+    let not_found_route = warp::any().map(|| {
+        warp::reply::with_status(
+            warp::reply::reply(),
+            warp::http::StatusCode::NOT_FOUND,
+        )
+    });
+
+    let routes = headers_route.or(not_found_route);
+
+    let value = env::var("PING_LISTEN_PORT").unwrap_or_else(|_| "3030".to_string()).parse().unwrap();
+
+    warp::serve(routes).run(([127, 0, 0, 1], value)).await;
+}
